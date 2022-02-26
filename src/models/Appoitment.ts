ada6321402030a9fea7003f8c5fe328ff8172cd3
Gabriel Schmidt Cordeiro <gabrielscordeiro2012@gmail.com>
2022-02-26 17:55:33 -0300

    refactor: 💡 Aplicando conceitos SOLID

---
+++ src/models/Appoitment.ts
@@ -0,0 +1,18 @@
+import { v4 as uuidv4 } from "uuid";
+
+class Appointment {
+  id: string;
+
+  provider: string;
+
+  date: Date;
+
+  // Omit pega todos os tipos(variáveis) dentro do objeto e remove um item
+  constructor({ provider, date }: Omit<Appointment, "id">) {
+    this.id = uuidv4();
+    this.provider = provider;
+    this.date = date;
+  }
+}
+
+export default Appointment;
---
+++ src/services/CreateAppointmentService.ts
@@ -0,0 +1,36 @@
+import { startOfHour } from "date-fns";
+import Appointment from "../models/Appoitment";
+import AppointmentsRepository from "../repository/AppointmentsRepository";
+
+interface RequestDTO {
+  provider: string;
+  date: Date;
+}
+
+class CreateAppointmentService {
+  private appointmentsRepository: AppointmentsRepository;
+
+  constructor(appointmentsRepository: AppointmentsRepository) {
+    this.appointmentsRepository = appointmentsRepository;
+  }
+
+  public execute({ date, provider }: RequestDTO): Appointment {
+    const appointmentDate = startOfHour(date);
+
+    const findAppointmentinSameDate =
+      this.appointmentsRepository.findByDate(appointmentDate);
+
+    if (findAppointmentinSameDate) {
+      throw Error("This appointment is already booked");
+    }
+
+    const appointment = this.appointmentsRepository.create({
+      provider,
+      date: appointmentDate,
+    });
+
+    return appointment;
+  }
+}
+
+export default CreateAppointmentService;
